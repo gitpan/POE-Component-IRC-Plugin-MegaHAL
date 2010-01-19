960ad0d27c1a77fd28a4294c1352c24273d8d5c5
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2010-01-19 23:50:59 +0000

    Import of HINRIK/POE-Component-IRC-Plugin-MegaHAL-0.30 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-MegaHAL
    gitpan-cpan-version:      0.30
    gitpan-cpan-path:         HINRIK/POE-Component-IRC-Plugin-MegaHAL-0.30.tar.gz
    gitpan-cpan-author:       HINRIK
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/MegaHAL.pm
+++ lib/POE/Component/IRC/Plugin/MegaHAL.pm
@@ -9,7 +9,7 @@ use POE::Component::AI::MegaHAL;
 use POE::Component::IRC::Common qw(l_irc matches_mask_array irc_to_utf8 strip_color strip_formatting);
 use POE::Component::IRC::Plugin qw(PCI_EAT_NONE);
 
-our $VERSION = '0.29';
+our $VERSION = '0.30';
 
 sub new {
     my ($package, %args) = @_;
@@ -147,7 +147,7 @@ sub _greet_handler {
     return;
 }
 
-sub normalize_megahal {
+sub _normalize_megahal {
     my ($self, $line) = @_;
 
     $line = decode('utf8', $line);

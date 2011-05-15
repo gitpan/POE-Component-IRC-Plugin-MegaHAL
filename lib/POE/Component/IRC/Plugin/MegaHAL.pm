5c820e508880d903ca3b5cfdd59094e2920dcec6
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2011-05-15 14:38:00 +0000

    Import of HINRIK/POE-Component-IRC-Plugin-MegaHAL-0.45 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-MegaHAL
    gitpan-cpan-version:      0.45
    gitpan-cpan-path:         HINRIK/POE-Component-IRC-Plugin-MegaHAL-0.45.tar.gz
    gitpan-cpan-author:       HINRIK
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/MegaHAL.pm
+++ lib/POE/Component/IRC/Plugin/MegaHAL.pm
@@ -3,7 +3,7 @@ BEGIN {
   $POE::Component::IRC::Plugin::MegaHAL::AUTHORITY = 'cpan:HINRIK';
 }
 BEGIN {
-  $POE::Component::IRC::Plugin::MegaHAL::VERSION = '0.44';
+  $POE::Component::IRC::Plugin::MegaHAL::VERSION = '0.45';
 }
 
 use strict;
